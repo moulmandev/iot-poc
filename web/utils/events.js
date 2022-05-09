e0750e0dfb889c153f7e472a76e54950c22861d5
chenehomard34 <gddi01@hotmail.fr>
2022-05-09 10:27:50 +0200

    good routes

--- web/utils/events.js
+++ web/utils/events.js
@@ -1,20 +1,13 @@
 module.exports = {
-    //CLIENT
-    GET_ENGINE: "/getEngine",
-    ADD_ENGINE: "/addEngine",
+    //FRONT
+    GET_ENGINE: "/getEngine", // Front call api to get all engines from database
+    ADD_ENGINE: "/addEngine", // Front call api to add new arduino with MAC ADDRESS to database
+
     
-    GET_AMBIANTE_TEMPERATURE: "/getChauffageTemperature",
-
-    SET_CHAUFFAGE_TEMPERATURE: "/setChauffageTemperature",
-    GET_CHAUFFAGE_TEMPERATURE: "/getChauffageTemperature",
-
-    SET_TEMPS_VOULU: "/setTempsVoulu",
-    GET_TEMPS_VOULU: "/getTempsVoulu",
-
-    SET_REGLE_VOULU: "/setRegleVoulu",
-    GET_REGLE_VOULU: "/getRegleVoulu",
-
-    SET_TEMPS_ACTUELLE: "/setTempsActuelle",
-    GET_TEMPS_ACTUELLE: "/getTempsActuelle",
+    //ARDUINO
+    // All calls must contain arduino MAC ADRESS to identify him
+    SET_AMBIANTE_TEMPERATURE: "/setAmbianteTemperature",  // Arduino call api pour dire quelle temperature il fait actuellement
+    GET_CHAUFFAGE_TEMPERATURE: "/getChauffageTemperature", // Arduino call api pour savoir la temperature à mettre
+    GET_REGLE_VOULU: "/getRegleVoulu", // Arduino call api pour savoir les règles d'activation
+    GET_TIME: "/getTime" // Arduino call api to know the current date/time
 };
-
